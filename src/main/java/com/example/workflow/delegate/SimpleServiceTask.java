a0609780982ff6837446284d79bd54097cb20107
Fedir Pyshnyi <fed.lviv@gmail.com>
2023-05-03 22:15:03 +0300

    Add BpmnError and Error Boundary Event

--- src/main/java/com/example/workflow/delegate/SimpleServiceTask.java
+++ src/main/java/com/example/workflow/delegate/SimpleServiceTask.java
@@ -1,6 +1,7 @@
 package com.example.workflow.delegate;
 
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,11 @@ public class SimpleServiceTask implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         String name = (String) delegateExecution.getVariable("firstName");
+
+        if (name == null) {
+            log.error("Error - firstName is required");
+            throw new BpmnError("error.name.null");
+        }
 
         log.info("{} call simple service task", name);
     }
